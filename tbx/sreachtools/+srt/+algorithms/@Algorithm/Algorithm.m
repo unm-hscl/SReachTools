a278865cc5f3c512c70ab8cc70853acf8033253d
adam <adam.thorpe.g@gmail.com>
2019-09-10 17:33:11 -0600

    Added verbose print function.

--- tbx/sreachtools/+srt/+algorithms/@Algorithm/Algorithm.m
+++ tbx/sreachtools/+srt/+algorithms/@Algorithm/Algorithm.m
@@ -3,9 +3,19 @@ classdef (Abstract) Algorithm < handle
 %
 %   Copyright 2019 Adam Thorpe
 
+    properties (Access = private)
+        verbose_(1, 1) double {mustBeNonnegative} = 0;
+    end
+
     methods
         function obj = Algorithm(varargin)
             % ALGORITHM Construct an instance of the algorithm.
+
+            p = inputParser;
+            addParameter(p, 'verbose', 0);
+            parse(p, varargin{:});
+
+            obj.verbose_ = p.Results.verbose;
 
             % Validate algorithm dependencies.
             obj.validatedependencies();
@@ -27,19 +37,28 @@ classdef (Abstract) Algorithm < handle
     end
 
     methods
-        function result = compute_point(obj, varargin)
+        function results = compute_point(obj, varargin)
             % COMPUTE_POINT Template method for algorithm.
             warning('compute_point not implemented for %s.', class(obj));
         end
 
-        function result = compute_set(obj, varargin)
+        function results = compute_set(obj, varargin)
             % COMPUTE_SET Template method for algorithm.
             warning('compute_set not implemented for %s.', class(obj));
         end
 
-        function result = compute_fwd(obj, varargin)
+        function results = compute_fwd(obj, varargin)
             % COMPUTE_FWD Template method for algorithm.
             warning('compute_fwd not implemented for %s.', class(obj));
+        end
+    end
+
+    methods (Access = protected)
+        function print_verbose(obj, level, varargin)
+            % print_verbose Prints verbose messages to the command window.
+            if obj.verbose_ >= level
+                fprintf(varargin{:});
+            end
         end
     end
 
