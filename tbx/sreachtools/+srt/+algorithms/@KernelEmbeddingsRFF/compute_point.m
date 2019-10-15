5f7eaf51c6941dd13ae294e2bd63cb5ad3c10941
adam <adam.thorpe.g@gmail.com>
2019-10-15 17:57:14 -0600

    Updating RFF implementation.

--- tbx/sreachtools/+srt/+algorithms/@KernelEmbeddingsRFF/compute_point.m
+++ tbx/sreachtools/+srt/+algorithms/@KernelEmbeddingsRFF/compute_point.m
@@ -18,18 +18,33 @@ M = sys.length;
 t_start = tic;
 
 % Compute random fourier features.
-wx = (1/obj.Sigma^2).*randn(obj.D, size(sys.X, 1));
-wu = (1/obj.Sigma^2).*randn(obj.D, size(sys.U, 1));
+% wx = (1/obj.Sigma).*randn(obj.D, size(sys.X, 1));
+% wu = (1/obj.Sigma).*randn(obj.D, size(sys.U, 1));
+% 
+% Zx = exp(1i*wx*sys.X);
+% Zu = exp(1i*wu*sys.U);
+% Zy = exp(1i*wx*sys.Y);
 
-Zx = exp(1i*wx*sys.X);
-Zu = exp(1i*wu*sys.U);
-Zy = exp(1i*wx*sys.Y);
+wx = (1/obj.Sigma)*randn(obj.D, size(sys.X, 1));
+wu = (1/obj.Sigma)*randn(obj.D, size(sys.U, 1));
 
-Z = Zx.*Zu;
+bx = (2*pi)*rand(obj.D, 1);
+bu = (2*pi)*rand(obj.D, 1);
 
-W = Z*Z' + obj.Lambda*eye(obj.D);
+Zx = (sqrt(2)/sqrt(obj.D))*cos(wx*sys.X + bx);
+Zu = (sqrt(2)/sqrt(obj.D))*cos(wu*sys.U + bu);
+Zy = (sqrt(2)/sqrt(obj.D))*cos(wx*sys.Y + bx);
+
+Z = Zx;
+
+H = Z*Z.';
+W = H + obj.Lambda*eye(obj.D); %#ok<*MINV>
 
 beta = W\Z;
+gamma = beta.'*Zy;
+
+% beta = normalize(beta);
+% beta = beta./sum(beta, 1);
 
 % Compute value functions.
 Vk = zeros(N, M);
@@ -43,7 +58,7 @@ switch class(prb)
             Vk(k, :) = prb.TargetTube.contains(k, sys.Y) + ...
                        (prb.ConstraintTube.contains(k, sys.Y) & ...
                         ~prb.TargetTube.contains(k, sys.Y)).* ...
-                            (Vk(k+1, :)*beta'*(Zy.*Zu));
+                            (Vk(k+1, :)*gamma);
         end
 
     case 'srt.problems.TerminalHitting'
@@ -52,7 +67,7 @@ switch class(prb)
 
         for k = N-1:-1:2
             Vk(k, :) = prb.ConstraintTube.contains(k, sys.Y).* ...
-                (Vk(k+1, :)*beta'*(Zy.*Zu));
+                (Vk(k+1, :)*gamma);
         end
 
     case 'srt.problems.Viability'
@@ -61,7 +76,7 @@ switch class(prb)
 
         for k = N-1:-1:2
             Vk(k, :) = prb.ConstraintTube.contains(k, sys.Y).* ...
-                (Vk(k+1, :)*beta'*(Zy.*Zu));
+                (Vk(k+1, :)*gamma);
         end
 
 end
@@ -69,8 +84,16 @@ end
 % Compute probabilities for point.
 Pr = zeros(N, mt);
 
-Zx0 = exp(1i*wx*x0);
-Zu0 = exp(1i*wu*u0);
+% Zx0 = exp(1i*wx*x0);
+% Zu0 = exp(1i*wu*u0);
+Zx0 = (sqrt(2)/sqrt(obj.D))*cos(wx*x0 + bx);
+Zu0 = (sqrt(2)/sqrt(obj.D))*cos(wu*u0 + bu);
+
+% beta = W*Z;
+gamma = beta.'*Zx0;
+
+% beta = normalize(beta, 'norm');
+% beta = beta./sum(abs(beta), 1);
 
 switch class(prb)
     case 'srt.problems.FirstHitting'
@@ -81,7 +104,7 @@ switch class(prb)
             Pr(k, :) = prb.TargetTube.contains(k, x0) + ...
                        (prb.ConstraintTube.contains(k, x0) & ...
                         ~prb.TargetTube.contains(k, x0)).* ...
-                            (Vk(k+1, :)*beta'*(Zx0.*Zu0));
+                            (Vk(k+1, :)*gamma);
         end
 
     case 'srt.problems.TerminalHitting'
@@ -90,7 +113,7 @@ switch class(prb)
 
         for k = N-1:-1:1
             Pr(k, :) = prb.ConstraintTube.contains(k, x0).* ...
-                (Vk(k+1, :)*beta'*(Zx0.*Zu0));
+                (Vk(k+1, :)*gamma);
         end
 
     case 'srt.problems.Viability'
@@ -99,7 +122,7 @@ switch class(prb)
 
         for k = N-1:-1:1
             Pr(k, :) = prb.ConstraintTube.contains(k, x0).* ...
-                (Vk(k+1, :)*beta'*(Zx0.*Zu0));
+                (Vk(k+1, :)*gamma);
         end
 
 end
